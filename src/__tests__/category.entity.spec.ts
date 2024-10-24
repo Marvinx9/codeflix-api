99e950757480eb28e318a77d9788d35de2235620
Afranio <afraniodantas0224@gmail.com>
2024-10-24 19:55:04 -0300

    test: entity category

---
+++ src/__tests__/category.entity.spec.ts
@@ -0,0 +1,48 @@
+import { Category } from '../domain/category.entity';
+
+describe('Category Unit Tests', () => {
+  it('Should create a new category', () => {
+    // Arrange & Act
+    const category = new Category({
+      name: 'Movie',
+    });
+
+    // Assert
+    expect(category.category_id).toBeUndefined();
+    expect(category.name).toBe('Movie');
+    expect(category.description).toBeNull();
+    expect(category.is_active).toBeTruthy();
+    expect(category.created_at).toBeInstanceOf(Date);
+  });
+
+  it('Should create a new category is_activate', () => {
+    // Arrange & Act
+    const category = new Category({
+      name: 'Movie',
+      is_active: false,
+    });
+
+    // Assert
+    expect(category.category_id).toBeUndefined();
+    expect(category.name).toBe('Movie');
+    expect(category.description).toBeNull();
+    expect(category.is_active).toBeFalsy();
+    expect(category.created_at).toBeInstanceOf(Date);
+  });
+
+  it('Should change name', () => {
+    // Arrange & Act
+    const category = new Category({
+      name: 'Movie',
+      is_active: false,
+    });
+    category.changeName('other name');
+
+    // Assert
+    expect(category.category_id).toBeUndefined();
+    expect(category.name).toBe('other name');
+    expect(category.description).toBeNull();
+    expect(category.is_active).toBeFalsy();
+    expect(category.created_at).toBeInstanceOf(Date);
+  });
+});
